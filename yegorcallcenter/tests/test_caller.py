5751795d98fdbab8c82cc9274a6fb984c5ab6323
kmueller <chaspar@web.de>
2023-02-08 10:36:12 -0800

    refactor: add default parameters to constructor

--- yegorcallcenter/tests/test_caller.py
+++ yegorcallcenter/tests/test_caller.py
@@ -4,3 +4,7 @@ import yegorcallcenter.Caller as Caller
 def test_caller_creation():
     a1 = Caller.Caller("curt")
     assert a1._name == "curt"
+    
+def test_caller_creation2():
+    a2 = Caller.Caller()
+    assert a2._name == a2._id
