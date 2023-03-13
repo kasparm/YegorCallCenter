2783b0284d3062b5c7a4f0fbbdb2eca20f123e1d
kmueller <chaspar@web.de>
2023-03-13 14:37:13 -0700

    feat: Create interface for issue and adjust tests

--- yegorcallcenter/Issue.py
+++ yegorcallcenter/Issue.py
@@ -1,17 +1,37 @@
+import abc
 import random
 
 
-class Issue:
+class Issue(metaclass=abc.ABCMeta):
     def __init__(self) -> None:
-        """Description of issue. Is issue resolved or not
-
-        Args:
-            id (_type_): _description_
-        """
         self._id = id
         # 0 -> not resolved, 1 -> resolved
         self._status = 0
+        self._difficulty = None
+
+    @abc.abstractmethod
+    def set_resolved(self):
+        pass
+
+    @abc.abstractmethod
+    def get_status(self):
+        pass
+
+    @abc.abstractmethod
+    def get_difficulty(self):
+        pass
+
+
+class BasicIssue(Issue):
+    def __init__(self) -> None:
+        super().__init__()
         self._difficulty = random.choice([0, 1, 2])
+
+    """Description of issue. Is issue resolved or not
+
+    Args:
+        id (_type_): _description_
+    """
 
     def set_resolved(self):
         self._status = 1
